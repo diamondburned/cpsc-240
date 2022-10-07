43ab479736473862796c6c7d83e082ddb701d5a4
diamondburned <diamond@arikawa-hi.me>
2022-10-06 23:05:40 -0700

    Add extra gcc/g++ flags

--- build.sh
+++ build.sh
@@ -7,20 +7,21 @@ glob() {
 	[ -e "$1" ] && echo "$@"
 }
 
-CFLAGS+="-g -m64 -fsanitize=address -fstack-protector -fno-pie -no-pie "
-CXXFLAGS+="$CFLAGS "
-CXXFLAGS+="-std=c++17 "
+CFLAGS="-g -m64 -Wall -Wextra -Wpedantic -fsanitize=address -fstack-protector -fno-pie -no-pie"
+CXXFLAGS="$CFLAGS -std=c++17"
 
 for f in $(glob *.asm); do
 	nasm -f elf64 -o "$f.o" "$f"
 done
 
 for f in $(glob *.cpp); do
-	g++ -c -Wall $CXXFLAGS -o "$f.o" "$f"
+	g++ -c $CXXFLAGS -o "$f.o" "$f"
 done
 
 for f in $(glob *.c); do
-	gcc -c -Wall $CFLAGS -o "$f.o" "$f"	
+	gcc -c $CFLAGS -o "$f.o" "$f"	
 done
 
 g++ $CXXFLAGS -o "$(basename "$PWD").out" ./*.o
+
+rm -f -- *.o *.lis
