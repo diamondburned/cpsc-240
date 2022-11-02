fd4b22c720dc467573dfbef47146f0a6e38726c4
diamondburned <diamond@arikawa-hi.me>
2022-11-02 15:12:10 -0700

    Improve $LD in build.sh

--- build.sh
+++ build.sh
@@ -15,28 +15,31 @@ glob() {
 	done
 }
 
-CFLAGS="-g -m64 -Wall -Wextra -Wpedantic -fsanitize=address -fstack-protector -fno-pie -no-pie"
-CXXFLAGS="$CFLAGS -std=c++17"
+CFLAGS="-g -m64 -Wall -Wextra -Wpedantic -fstack-protector -fno-pie -no-pie"
+CXXFLAGS="$CFLAGS -std=c++17 -lstdc++"
 
-hasC=
+LD="ld"
 out="$(basename "$PWD").out"
 
 for f in $(glob *.asm); do
 	nasm -f elf64 -F dwarf -g -O0 -o "$f.o" "$f"
 done
 
-for f in $(glob *.cpp); do
-	hasC=1
-	g++ -c $CXXFLAGS -o "$f.o" "$f"
-done
-
 for f in $(glob *.c); do
+	LD="gcc"
 	hasC=1
 	gcc -c $CFLAGS -o "$f.o" "$f"	
 done
 
-[ -n "$hasC" ] \
-	&& gcc $CXXFLAGS -o $out *.o \
-	|| ld -o $out *.o
+for f in $(glob *.cpp); do
+	LD="g++"
+	g++ -c $CXXFLAGS -o "$f.o" "$f"
+done
+
+case "$LD" in
+	ld)  ld -o $out *.o;;
+	gcc) gcc $CFLAGS -o $out *.o;;
+	g++) g++ $CXXFLAGS -o $out *.o;;
+esac
 
 rm -f -- *.o *.lis
